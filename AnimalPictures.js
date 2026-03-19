e0c0e588e313e9232f91137d9b87108787ac7c4c
Elia Schwartz <pandababy12@att.net>
2026-03-19 15:41:43 -0400

    Add Apache program files including pictures

---
+++ AnimalPictures.js
@@ -0,0 +1,40 @@
+document.getElementById("pandaButton").addEventListener("click", function(){
+	if (document.getElementById("pandaImage").style.display === "none"){
+		document.getElementById("pandaImage").style.display = "block";
+	}
+
+});
+
+document.getElementById("dogButton").addEventListener("click", function(){
+	if (document.getElementById("dogImage").style.display === "none"){
+		document.getElementById("dogImage").style.display = "block";
+	}
+});
+
+document.getElementById("catButton").addEventListener("click", function(){
+	if (document.getElementById("catImage").style.display === "none"){
+		document.getElementById("catImage").style.display = "block";
+	}
+});
+
+document.getElementById("otterButton").addEventListener("click", function(){
+	if (document.getElementById("otterImage").style.display === "none"){
+		document.getElementById("otterImage").style.display = "block";
+	}
+});
+
+document.getElementById("monkeyButton").addEventListener("click", function(){
+	if (document.getElementById("monkeyImage").style.display === "none"){
+		document.getElementById("monkeyImage").style.display = "block";
+
+	}
+});
+
+document.getElementById("turtleButton").addEventListener("click", function(){
+	if(document.getElementById("turtleImage").style.display === "none"){
+		document.getElementById("turtleImage").style.display = "block";
+	}
+});
+
+
+
